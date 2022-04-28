ca269319d567a5e555da94d745f0e5aa3e0bd8dd
taylorrk <taylorrk@live.unc.edu>
2022-04-28 16:10:53 -0400

    yuhhh

--- flips.js
+++ flips.js
@@ -1,27 +1,9 @@
-import { coinFlip, countFlips, coinFlips, flipACoin } from './modules/coin.mjs';
+import { coinFlips, countFlips } from "./modules/coin.mjs";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const args = require('minimist')(process.argv.slice(2))
+const number = args.number || 1
 
-import minimist from 'minimist'; 
-
-if (process.argv.length < 3) {
-    console.log(coinFlip());
-}
-else {
-    const myArgs = process.argv.slice(2);
-    const myArgs1 = myArgs[0];
-    var myArgs2 = myArgs1.charAt(9);
-    if (myArgs2 == null) {
-        console.log(coinFlip());
-    }
-    else {
-        var i = 10;
-        while (i < myArgs1.length) {
-            myArgs2 = myArgs2 + myArgs1.charAt(i);
-            i = i + 1;
-        }
-        var final = parseInt(myArgs2);
-        var listOfCoins = coinFlips(final);
-        console.log(listOfCoins);
-        var final = countFlips(listOfCoins);
-        console.log(final);
-    }
-}
+const result = coinFlips(args.number);
+console.log(result);
+console.log(countFlips(result));
